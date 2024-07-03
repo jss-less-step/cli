0cb8cbc21a0802fac79f997453ddd80622fa9b51
姜鑫 <1599925910@qq.com>
2024-07-03 19:47:14 +0800

    修改importLocal路径

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,7 +1,7 @@
 import importLocal from "import-local";
 import logger from "@jss-cli/utils-log";
 import core from "../lib/index.js";
-if (importLocal(import.meta.url)) {
+if (importLocal(__filename)) {
 	logger.info("使用本地加载路径");
 } else {
 	core(process.argv);
